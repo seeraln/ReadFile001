f7624e1de80776b20b552c4220bd8a15658f4530
Seeralan Saththiyaseelan <24449@student.dorset-college.ie>
2021-10-20 17:20:40 +0100

    Ajoutez des fichiers projet.

---
+++ ReadFile001/FileDemo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace ReadFile001
+{
+    class FileDemo
+    {
+        public static void ReadAndParseString(string file)
+        {
+            string path = "C:/Users/seera/OneDrive/Bureau/Ecole/A3/S5/Object oriented";
+            //string file = "jamaica.txt";
+            string fileToRead = $"{path}/{file}";
+
+            Console.WriteLine($"Reading {fileToRead}");
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+
+                    //Console.WriteLine($"Starting to Reading {fileToRead}");
+
+                    string line;
+
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] splitData = line.Split(":");
+
+                        Console.WriteLine($"\t{String.Format("{0,-30}", splitData[0])}\t{splitData[1]}");
+
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+
+            }
+        }
+
+        public static void WriteDemo(string file, string[] heroes)
+        {
+
+
+            string path = "C:/Users/seera/OneDrive/Bureau/Ecole/A3/S5/Object oriented";
+            // string file = "heroes.txt";
+            string fileToWrite = $"{path}/{file}";
+
+            using (StreamWriter sw = new StreamWriter(fileToWrite))
+            {
+                foreach (string s in heroes)
+                {
+                    sw.WriteLine(s);
+                }
+
+            }
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+
+        public static char[]  SplitAString(string stringToSplit)
+        {
+            char[] characters = stringToSplit.ToCharArray();
+            foreach(char c in characters)
+            {
+                Console.Write($"'{c}'");
+            }
+            Console.WriteLine("");
+            return characters;
+        }
+
+        public static void OutputSep()
+        {
+            Console.WriteLine("=".PadRight(80, '='));
+        }
+       
+    }
+}
---
+++ ReadFile001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFile001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // FileDemo.ReadDemo("jamaica.txt");
+            string dc_heroesFile = "dc-heroes.txt";
+            string[] dc_heroes = { "Batman: Bruce Wayne", "Flash : jean ", "Martian Manhunter: pierre" };
+            FileDemo.WriteDemo(dc_heroesFile, dc_heroes);
+            FileDemo.ReadAndParseString(dc_heroesFile);
+
+            FileDemo.OutputSep();
+
+
+            string marvel_heroesFile = " marvel-heroes.txt";
+            string[] marvel_heroes = { "Iron-Man: Tony Stark", "Hulk:Bruce Banner", "Thor: Thor" };
+            FileDemo.WriteDemo(marvel_heroesFile, marvel_heroes);
+            FileDemo.ReadAndParseString(marvel_heroesFile);
+
+            char[] characters = FileDemo.SplitAString("supercalifragilisticexpialidocious");
+            Console.WriteLine($"The character at position 11 is {characters[10]}");
+        }
+    }
+}
